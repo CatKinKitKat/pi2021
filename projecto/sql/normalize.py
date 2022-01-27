48eb270655b0b89ba4a12cd96d2421f9edea5fa2
17440@stu.ipbeja.pt <goncalo.cand.amaro@gmail.com>
2022-01-27 19:33:48 +0000

    DB for dated/timed analysis

---
+++ projecto/sql/normalize.py
@@ -0,0 +1,75 @@
+import os
+import re
+import unicodedata
+
+
+def main():
+    current_dir = os.getcwd()
+    normalize_files(current_dir + "/data/activities/")
+    normalize_files(current_dir + "/data/hotels/")
+    normalize_files(current_dir + "/data/restaurants/")
+    normalize_files(current_dir + "/dates/activities/")
+    normalize_files(current_dir + "/dates/hotels/")
+    normalize_files(current_dir + "/dates/restaurants/")
+    normalize_files(current_dir + "/keywords/activities/")
+    normalize_files(current_dir + "/keywords/hotels/")
+    normalize_files(current_dir + "/keywords/restaurants/")
+
+
+def normalize_files(directory):
+    for file in os.listdir(directory):
+        if file.endswith(".csv"):
+            normalize_file(directory + file)
+
+
+def normalize_file(file_name):
+    lines = []
+    with open(file_name, "r", encoding="utf-8", errors="ignore") as f:
+        lines = f.readlines()
+    with open(file_name, "w", encoding="utf-8", errors="ignore") as f:
+        for line in lines:
+            # f.write(normalize(remove_index(line)))
+            f.write(normalize(line))
+
+
+def normalize(text):
+    text = remove_emoji(text)
+    text = (
+        unicodedata.normalize("NFKD", text)
+        .encode("ascii", "ignore")
+        .decode("utf-8", "ignore")
+    )
+    return text
+
+
+# def remove_index(string):
+#     return re.sub(r"^[^,]*,", "", string)
+def remove_emoji(string):
+    emoji_pattern = re.compile(
+        "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642"
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+        "]+",
+        flags=re.UNICODE,
+    )
+    return re.sub(emoji_pattern, "", string)
+
+
+if __name__ == "__main__":
+    main()
---
+++ projecto/sql/sql.py
@@ -0,0 +1,274 @@
+import os
+import re
+from numpy import place
+import pandas
+import sqlite3
+
+
+def create_db(db_name):
+    conn = sqlite3.connect(db_name)
+    return conn
+
+
+def create_table(conn, table_name, cols):
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS {} {}".format(table_name, cols))
+    conn.commit()
+
+
+def insert_data(conn, table_name, data):
+    c = conn.cursor()
+    c.execute("INSERT INTO {} VALUES {}".format(table_name, data))
+    conn.commit()
+
+
+def export_data(conn, table_name, file_name):
+    c = conn.cursor()
+    c.execute("SELECT * FROM {}".format(table_name))
+    data = c.fetchall()
+    df = pandas.DataFrame(data)
+    df.to_csv(file_name, index=False)
+
+
+def main():
+    # Create database
+    db_name = "projecto.db"
+    conn = create_db(db_name)
+
+    # Create table
+    table_name = "business_type"
+    cols = "(business_type_id INTEGER PRIMARY KEY, business_type_name TEXT)"
+    create_table(conn, table_name, cols)
+
+    # Create table
+    table_name = "business"
+    cols = "(business_id INTEGER PRIMARY KEY, business_name TEXT, business_type_id INTEGER, FOREIGN KEY (business_type_id) REFERENCES business_type (business_type_id))"
+    create_table(conn, table_name, cols)
+
+    # Create table
+    table_name = "reviews"
+    cols = "(review_id INTEGER PRIMARY KEY, review_text TEXT, business_id INTEGER, sentiment TEXT, keywords TEXT, month TEXT, year INTEGER, FOREIGN KEY (business_id) REFERENCES business (business_id))"
+    create_table(conn, table_name, cols)
+
+    # Insert data
+    table_name = "business_type"
+    data = "(1, 'Restaurant')"
+    insert_data(conn, table_name, data)
+    data = "(2, 'Attraction')"
+    insert_data(conn, table_name, data)
+    data = "(3, 'Hotel')"
+    insert_data(conn, table_name, data)
+
+    # Insert data
+    table_name = "business"
+    pd = pandas.read_csv(
+        "./data/restaurants/listtable.csv", engine="python", on_bad_lines="skip"
+    )
+    pd.columns = pd.columns.str.strip()
+    lst = []
+    lst = pd["Restaurante"].tolist()
+    l = 1
+    for i in range(len(lst)):
+        data = (
+            "("
+            + str(l)
+            + ", '"
+            + str(lst[i]).encode("ascii", "ignore").decode("utf-8", "ignore")
+            + "', 1)"
+        )
+        insert_data(conn, table_name, data)
+        l += 1
+    pd = pandas.read_csv(
+        "./data/activities/listtable.csv", engine="python", on_bad_lines="skip"
+    )
+    pd.columns = pd.columns.str.strip()
+    lst = pd["Attraction"].tolist()
+    for i in range(len(lst)):
+        data = (
+            "("
+            + str(l)
+            + ", '"
+            + str(lst[i]).encode("ascii", "ignore").decode("utf-8", "ignore")
+            + "', 2)"
+        )
+        insert_data(conn, table_name, data)
+        l += 1
+    pd = pandas.read_csv(
+        "./data/hotels/listtable.csv", engine="python", on_bad_lines="skip"
+    )
+    pd.columns = pd.columns.str.strip()
+    lst = pd["Hotel"].tolist()
+    for i in range(len(lst)):
+        data = (
+            "("
+            + str(l)
+            + ", '"
+            + str(lst[i]).encode("ascii", "ignore").decode("utf-8", "ignore")
+            + "', 3)"
+        )
+        insert_data(conn, table_name, data)
+        l += 1
+
+    # Insert data
+    table_name = "reviews"
+    for file in os.listdir("./data/activities"):
+        if file.startswith("place") and not file.endswith(".zip"):
+            print(file)
+            pd = pandas.read_csv(
+                "./data/activities/" + file, engine="python", on_bad_lines="skip"
+            )
+            pd.columns = pd.columns.str.strip()
+            lst = pd["review"].tolist()
+            lsts = pd["sentiment"].tolist()
+            pd = pandas.read_csv(
+                "./dates/activities/" + file, engine="python", on_bad_lines="skip"
+            )
+            pd.columns = pd.columns.str.strip()
+            lstm = pd[pd.columns[0]].tolist()
+            lsty = pd[pd.columns[1]].tolist()
+            pd = pandas.read_csv(
+                "./keywords/activities/" + file, engine="python", on_bad_lines="skip"
+            )
+            pd.columns = pd.columns.str.strip()
+            lstk = pd["Expressao"].tolist()
+            for i in range(len(lst)):
+                try:
+                    insert_data(
+                        conn,
+                        table_name,
+                        "(NULL, '"
+                        + str(lst[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + "', 3, '"
+                        + str(lsts[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + "', '"
+                        + str(lstk[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + "', '"
+                        + str(lstm[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + "', "
+                        + str(lsty[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + ")",
+                    )
+                except:
+                    pass
+    for file in os.listdir("./data/hotels"):
+        if file.startswith("hotel") and not file.endswith(".zip"):
+            print(file)
+            pd = pandas.read_csv(
+                "./data/hotels/" + file, engine="python", on_bad_lines="skip"
+            )
+            pd.columns = pd.columns.str.strip()
+            lst = pd["review"].tolist()
+            lsts = pd["sentiment"].tolist()
+            pd = pandas.read_csv(
+                "./dates/hotels/" + file, engine="python", on_bad_lines="skip"
+            )
+            pd.columns = pd.columns.str.strip()
+            lstm = pd[pd.columns[0]].tolist()
+            lsty = pd[pd.columns[1]].tolist()
+            pd = pandas.read_csv(
+                "./keywords/hotels/" + file, engine="python", on_bad_lines="skip"
+            )
+            pd.columns = pd.columns.str.strip()
+            lstk = pd["Expressao"].tolist()
+            for i in range(len(lst)):
+                try:
+                    insert_data(
+                        conn,
+                        table_name,
+                        "(NULL, '"
+                        + str(lst[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + "', 3, '"
+                        + str(lsts[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + "', '"
+                        + str(lstk[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + "', '"
+                        + str(lstm[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + "', "
+                        + str(lsty[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + ")",
+                    )
+                except:
+                    pass
+    for file in os.listdir("./data/restaurants"):
+        if file.startswith("restaurant") and not file.endswith(".zip"):
+            print(file)
+            pd = pandas.read_csv(
+                "./data/restaurants/" + file, engine="python", on_bad_lines="skip"
+            )
+            pd.columns = pd.columns.str.strip()
+            lst = pd["review"].tolist()
+            lsts = pd["sentiment"].tolist()
+            pd = pandas.read_csv(
+                "./dates/restaurants/" + file, engine="python", on_bad_lines="skip"
+            )
+            pd.columns = pd.columns.str.strip()
+            lstm = pd[pd.columns[0]].tolist()
+            lsty = pd[pd.columns[1]].tolist()
+            pd = pandas.read_csv(
+                "./keywords/restaurants/" + file, engine="python", on_bad_lines="skip"
+            )
+            pd.columns = pd.columns.str.strip()
+            lstk = pd["Expressao"].tolist()
+            for i in range(len(lst)):
+                try:
+                    insert_data(
+                        conn,
+                        table_name,
+                        "(NULL, '"
+                        + str(lst[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + "', 3, '"
+                        + str(lsts[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + "', '"
+                        + str(lstk[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + "', '"
+                        + str(lstm[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + "', "
+                        + str(lsty[i])
+                        .encode("ascii", "ignore")
+                        .decode("utf-8", "ignore")
+                        + ")",
+                    )
+                except:
+                    pass
+
+    conn.commit()
+
+    # export sqlite database to csv
+    export_data(conn, "reviews", "./reviews.csv")
+    export_data(conn, "business", "./business.csv")
+    export_data(conn, "business_type", "./business_type.csv")
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
+    print("Done")
